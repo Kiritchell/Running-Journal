722887d8bdadbd6895b3bcbdfcdb45ab3d9baf75
Kiritchell <kiritchell@gmail.com>
2023-05-28 20:47:34 -0500

    --all

---
+++ Running Posts.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+
+export let posts = [
+
+    {author:'Estetikk',
+    uuid:uuidv4(),
+    polaroidtitle:'HERMES',
+    date:"May 26, 2023",
+    collection:'where to start'},
+
+    {author:'Estetikk',
+    uuid:uuidv4(),
+    polaroidtitle:'MARATHON',
+    date:"May 4, 2023",
+    collection:'enduring'},
+
+    {author:'Estetikk',
+    uuid:uuidv4(),
+    polaroidtitle:'EAT RIGHT',
+    date:"April 18, 2023",
+    collection:'Food'}
+]
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('hello')
+
+
+import { posts } from "/Running Posts.js";
+const polaroidTitle = document.querySelector('.polaroid-title')
+let journalFeed = document.querySelector('.feed')
+
+
+console.log(posts[0].polaroidtitle)
+
+function render(){
+posts.forEach(function eachPost(post){
+    journalFeed.innerHTML += `
+    <a href="${post.uuid}">
+    <div class="post-wrapper">
+        <div class="polaroid-frame">
+          <div class="film">
+          </div>
+          <h3 class="polaroid-title">${post.polaroidtitle}</h3>
+          <div class="post-date-title">
+            <h6 class="date">${post.date}</h6>
+            <h6 class="title">${post.collection}</h6>
+            </div>
+        </div>
+      </div>
+      </a>`
+
+    console.log(post)
+})
+}
+
+render()
+
+
